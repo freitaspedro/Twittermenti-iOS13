c601d6894532d2e0f6a63dc0c016456349106fd7
pedro <pfreitas@id.uff.br>
2021-01-14 17:39:46 -0300

    add search tweet and swifty json lib

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -8,12 +8,16 @@
 
 import UIKit
 import SwifteriOS
+import CoreML
+import SwiftyJSON
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
+    
+    let sentimentClassifier = TweetSentimentClassifier()
     
     private var swifter: Swifter!
     
@@ -23,7 +27,6 @@ class ViewController: UIViewController {
                let secret = swifterSecrets?.object(forKey: "TWITTER_CONSUMER_SECRET") as? String {
                 self.swifter = Swifter(consumerKey: key, consumerSecret: secret)
             }
-
         }
     }
 
@@ -31,6 +34,28 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         getSecrets()
+        
+//        let prediction = try! sentimentClassifier.prediction(text: "@Apple is a terrible company!")
+//
+//        print(prediction.label)
+        
+        swifter.searchTweet(using: "@Apple", lang: "en", count: 100, tweetMode: .extended) { (results, metadata) in
+            
+            var tweets = [String]()
+            
+            for i in 0..<100 {
+                if let tweet = results[i]["full_text"].string {
+                    tweets.append(tweet)
+                }
+            }
+            
+            print(tweets)
+            
+            
+        } failure: { (error) in
+            print("There was an error with the Twitter API Request, \(error)")
+        }
+
         
     }
     
