c9faa51748525b2b0c5b215afd69e7792338a557
pedro <pfreitas@id.uff.br>
2021-01-14 14:55:25 -0300

    add swifter init

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -7,16 +7,37 @@
 //
 
 import UIKit
+import SwifteriOS
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
+    
+    private var swifter: Swifter!
+    
+    private var swifterSecrets: NSDictionary! {
+        didSet {
+            if let key = swifterSecrets?.object(forKey: "TWITTER_CONSUMER_KEY") as? String,
+               let secret = swifterSecrets?.object(forKey: "TWITTER_CONSUMER_SECRET") as? String {
+                self.swifter = Swifter(consumerKey: key, consumerSecret: secret)
+            }
+
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        getSecrets()
+        
+    }
+    
+    func getSecrets() {
+        if let path = Bundle.main.path(forResource: "Swifter", ofType: "plist") {
+            swifterSecrets = NSDictionary(contentsOfFile: path)
+        }
     }
 
     @IBAction func predictPressed(_ sender: Any) {
